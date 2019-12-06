e3b441e8fc58f10080e2ad5335fa5e3571bbec80
musgravej <jim.musgrave@gmail.com>
2019-12-06 17:12:15 -0600

    added support for completion email

--- pgp_decrypt.py
+++ pgp_decrypt.py
@@ -5,10 +5,14 @@ from datetime import datetime as dt
 import configparser
 import ftplib
 import csv
+import smtplib
+import ssl
+import pysftp
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 # TODO move completed file into completed folder
 # TODO query completed date
-# TODO send email
 
 
 class Globals:
@@ -21,11 +25,17 @@ class Globals:
         self.passphrase = config['PATHS']['passphrase']
         self.encrypt_date = '12/05/2019'
 
-        self.email_to = config['FTP_TEST']['email_to']
-        self.host = config['FTP_TEST']['host']
-        self.user = config['FTP_TEST']['user']
-        self.password = config['FTP_TEST']['password']
-        self.path = config['FTP_TEST']['path']
+        self.host = config['FTP']['host']
+        self.user = config['FTP']['user']
+        self.password = config['FTP']['password']
+        self.path = config['FTP']['path']
+        self.protocol = config['FTP']['protocol']
+
+        self.email_to = config['EMAIL']['email_to']
+        self.email_from = config['EMAIL']['email_from']
+        self.email_user = config['EMAIL']['email_user']
+        self.email_password = config['EMAIL']['email_password']
+        self.email_server = config['EMAIL']['email_server']
 
     def set_encrypt_date(self):
         pass
@@ -145,18 +155,65 @@ def convert_to_csv(search_path, txt_files):
     return success
 
 
-def transfer_to_ftp(file_list):
-    with ftplib.FTP(host=g.host) as ftp_conn:
-        ftp_conn.connect()
-        ftp_conn.login(user=g.user, passwd=g.password)
-        ftp_conn.cwd(g.path)
+def transfer_to_ftp(search_path, file_list):
+    if g.protocol == 'ftp':
+        with ftplib.FTP(host=g.host) as ftp_conn:
+            ftp_conn.connect()
+            ftp_conn.login(user=g.user, passwd=g.password)
+            ftp_conn.cwd(g.path)
 
-        for f in file_list:
-            with open(f, 'rb') as transfer:
-                print(f"Transferring {f}")
-                ftp_conn.storbinary(f'STOR {f}', transfer)
+            for f in file_list:
+                with open(os.path.join(search_path, f), 'rb') as transfer:
+                    print(f"Transferring {f}")
+                    ftp_conn.storbinary(f'STOR {f}', transfer)
 
-        print(ftp_conn.retrlines('LIST'))
+            print(ftp_conn.retrlines('LIST'))
+
+    if g.protocol == 'sftp':
+        with pysftp.Connection(host=g.host, username=g.user, password=g.password) as sftp_conn:
+            with sftp_conn.cd(g.path):
+                for f in file_list:
+                    sftp_conn.put(os.path.join(search_path, f))
+                for attr in sftp_conn.listdir_attr():
+                    print(attr.filename, attr)
+
+
+def send_email(csv_list):
+    port = 587
+    smtp_server = g.email_server
+    sender_email = g.email_user
+    email_from = g.email_from
+    receiver_email = g.email_to
+    password = g.email_password
+
+    fle_format = "file has" if len(csv_list) == 1 else "files have"
+    table_data = ""
+    for filename, records in csv_list:
+        table_data += f"<tr><td>{filename[:-4]}.txt</td><td>{records}</td></tr>"
+
+    text = "Transfer complete"
+
+    html = ("<html> <head> <style> td, th {{ border: 1px solid #dddddd; text-align: left; padding: 8px;}}"
+            "</style> </head> <body> <p>The following {fle_format} been been added to user groups:<br> "
+            "</p> <table width: 100%;> <tr> <th>File</th> <th>Records</th> </tr> "
+            "{table_data} </table> </body> </html> "
+            "<p>Matching files have been pgp encrypted and uploaded to "
+            "the FTP site</p>".format(table_data=table_data, fle_format=fle_format))
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = f"Add to new agency complete: {g.encrypt_date}"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    message.attach(MIMEText(text, "plain"))
+    message.attach(MIMEText(html, "html"))
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.set_debuglevel(1)
+        server.starttls()
+        server.login(sender_email, password)
+        server.sendmail(email_from, receiver_email, message.as_string())
 
 
 def run_decrypt():
@@ -169,8 +226,9 @@ def run_encrypt():
     txt_files = [f for f in os.listdir(search_path) if f.upper()[-3:] == 'TXT']
 
     csv_list = convert_to_csv(search_path, txt_files)
-    file_list = encrypt_txt(search_path, csv_list)
-    transfer_to_ftp(file_list)
+    # file_list = encrypt_txt(search_path, csv_list)
+    # transfer_to_ftp(search_path, file_list)
+    send_email(csv_list)
 
 
 def main():
